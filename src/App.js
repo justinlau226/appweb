1d28824136ba1ac112b132a5de479b8510c0cde9
jlau <justinlau226@gmail.com>
2022-06-10 12:25:40 -0700

    created aqiweb

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import tabledata from './tabledata.css'
+
+function TableData() {
+    const [data, getData] = useState([])
+    const URL = 'https://jsonplaceholder.typicode.com/posts';
+    const mainURL = 'http://forge.smartcampus.city/api/airquality/';
+ 
+    useEffect(() => {
+        fetchData()
+    }, [])
+ 
+ 
+    const fetchData = () => {
+        fetch(mainURL)
+            .then((res) =>
+                res.json())
+ 
+            .then((response) => {
+              let aqidata = response.sensor_readings
+              console.log(aqidata);
+              getData(aqidata);
+                
+            })
+ 
+    }
+ 
+    return (
+        <>
+            <h1>AQI DATA</h1>
+            <tbody>
+                <tr>
+                    <th>datetime</th>
+                    <th>id</th>
+                    <th>pm25</th>
+                    {/* <th>Description</th> */}
+                </tr>
+                {data.map((item, i) => (
+                    <tr key={i}>
+                        <td>{item.datetime}</td>
+                        <td>{item.id}</td>
+                        <td>{item.pm25}</td>
+                        {/* <td>{item.body}</td> */}
+                    </tr>
+                ))}
+            </tbody>
+ 
+        </>
+    );
+}
+ 
+export default TableData;
